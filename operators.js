8944e83d3284f363c195c735fe7e0458b52276f4
Sandhyag@123 <balasoghadage24167@gmail.com>
2023-10-02 07:48:23 +0530

    add

---
+++ operators.js
@@ -0,0 +1,149 @@
+// a=10;
+// console.log(a)
+// var a=10,b=20;
+// console.log(a+b)
+// var a=45;
+// console.log(a)
+// let aa=55;
+//  aa=56;
+// console.log(aa)
+// const c=57;
+// c=66;
+// console.log(c)
+// const f;
+// console.log(f)
+// var a=10,b=20;
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**2)
+// console.log(++a)
+// console.log(a++)
+// console.log(a)
+// console.log(--b)
+// console.log(b--)
+// console.log(b)
+// let i=10;
+// for(let a=1;a<=i;a++){
+//     console.log(a*2)
+// }
+// for(let a=1;a<=i;a+=2){
+//     console.log(a)
+// }
+// let a=10,b=3;
+// console.log(a>b&&b<a)
+// const aa=true,bb=false;
+// console.log(aa===bb&&bb===aa)
+// let a=-20,b=5;
+// console.log(a>b||b>a)
+// let a=10,b=5;
+// console.log(!(a>=b&&b<=a))
+// let f=0.5,g=0.6;
+// console.log(!(f>g||g>f))
+// let a=50,b=35;
+// console.log(a==b)
+// console.log(a===b)
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a>=b)
+// console.log(a<=b)
+// console.log(a>=b!=b<=a)
+// let a="Good",b="Good";
+// // console.log(a.length==b.length ? "A is Good" : "B is Morning")
+// if(a.length===b.length){
+//     console.log("A is A")
+// }
+// else{
+//     console.log("B is B")
+// }
+// let count=10;
+// for(let a=1;a<=count;a++){
+//     console.log(a*2)
+// }
+// let a=1;
+// while(a<=count){
+// console.log(a*2)
+// a++;
+// }
+// let count=10,a=1;
+// do{
+//     console.log(a*2);
+//     a++;
+// }
+// while(a<=count)
+
+// let count=10;
+// for(let i=10;i<=count;i++){
+//     console.log(i)
+// }
+
+// let c=10;
+// while(c<=count){
+//     console.log(c)
+//     c++;
+// }
+// let d=10;
+// do{
+//     console.log(d);
+//     d++;
+// }
+// while(d<=count)
+// let a=2,b=3;
+// console.log(a+=b) //5
+// console.log(a-=b) //5-3=2
+// console.log(a*=b) //2*3=6
+// console.log(a/=b) //6/3=2
+// console.log(a%=b) //2%3=
+// console.log(a**=b) //2**3=8
+// let day;
+// switch(new Date().getDay()){
+//     case 1:
+//         day="Monday";
+//         break;
+//     // case 1:
+//     //     day="Tuesday";
+//     //     break;
+//     case 2:
+//         day="Wednesday";
+//         break;
+//     case 3:
+//         day="Thursday";
+//         break;
+//     case 4:
+//         day="Friday";
+//         break;
+// }
+// console.log(day)
+
+// let a=0;
+// switch(a){
+//     case 0:
+//       console.log( "Sunday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//        console.log("Tuesday");
+//         break;
+//     case 3:
+//        console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Wrong");
+//     break;
+// }
+let day,a=0;
+switch (a) {
+    case 0:
+        console.log(day = "Mon");
+        break;
+    case 1:
+        console.log(day = "Tue");
+        break;
+    case 2:
+        console.log(day = "Wed");
+        break;
+    default:
+        console.log("Wrong");
+        break;
+}
