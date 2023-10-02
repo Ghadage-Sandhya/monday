f1f747f11e7fa905989b649ef29f910b3ee1e85e
Sandhyag@123 <balasoghadage24167@gmail.com>
2023-10-02 07:51:07 +0530

    Add

---
+++ Basic.js
@@ -0,0 +1,18 @@
+var a=10,b=3;
+// 1. Arithmetic operator:
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log("Mod",a%b)
+// console.log(a+b)
+// console.log(b++)
+// console.log(++b)
+// console.log(b)
+// console.log(a**5)
+// 2. Comparison operator:
+// console.log(a<b&&b>a)
+// console.log(a<b||b>a)
+// console.log(!(a<b||b<a))
+// console.log(!(a<b&&b>a))
+// 3. Comparison operator:
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+ // case 1:
+    //     day="Tuesday";
+    //     break;
